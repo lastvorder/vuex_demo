d2f349f20c837a9f0d250c78b788b187506377fa
lastorder <1102989789@qq.com>
2021-02-11 00:29:15 +0800

    修改后端url

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function request(config) {
   const instance = axios.create({
-    baseURL: "http://localhost:8081",
+    baseURL: "http://106.14.171.180:8081",
     timeout: 50000
   });
   return instance(config);
